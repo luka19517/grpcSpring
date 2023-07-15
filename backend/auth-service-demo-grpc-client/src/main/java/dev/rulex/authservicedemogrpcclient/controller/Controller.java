42eb38807b202de4038430b7b19b1e119110be37
luka19517 <luka.djorovic3010@gmail.com>
2023-07-15 12:19:46 +0200

    vite frontend added
    vite frontend added

--- backend/auth-service-demo-grpc-client/src/main/java/dev/rulex/authservicedemogrpcclient/controller/Controller.java
+++ backend/auth-service-demo-grpc-client/src/main/java/dev/rulex/authservicedemogrpcclient/controller/Controller.java
@@ -4,12 +4,10 @@ import dev.rulex.authservicedemogrpcclient.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth-api")
 @CrossOrigin
 public class Controller {
 
@@ -19,6 +17,12 @@ public class Controller {
     @GetMapping("/auth")
     public ResponseEntity<String> auth(@RequestParam("username") String username, @RequestParam("password") String password) {
         String result = authService.authorize(username, password);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(@RequestParam("name") String name) {
+        String result = "Hello from server: " + name;
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
---
+++ frontend/src/components/CustomComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>{{ result }}</h2>
+</template>
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const result = ref();
+
+async function testConnection() {
+
+    const response = await axios({
+        method: 'get',
+        url: '/auth-api/test?name=Luka',
+    })
+    result.value = response.data
+}
+
+testConnection();
+
+</script>
